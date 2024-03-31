b29514bfee9c9286fdd6f9612583d4f6374dc194
Veselov Max <veselov.maksim@lit1533.ru>
2024-03-31 19:51:24 +0300

    t

--- Project4/BaseArray.cs
+++ Project4/BaseArray.cs
@@ -2,7 +2,7 @@
 
 namespace Project4
 {
-    public abstract class BaseArray<T> : IBaseArray
+    public abstract class BaseArray<T>
     {
         public abstract void Print();
     }
--- Project4/OneDimensionalArray.cs
+++ Project4/OneDimensionalArray.cs
@@ -1,22 +1,25 @@
 ﻿using System;
-using System.ComponentModel;
 
 namespace Project4
 {
     public sealed class OneDimensionalArray<T> : BaseArray<T>
-    {
+        where T : IComparable<T>
+    {        
         private T[] array;
 
+        public int size;
+
+        private const int defaultCapacity = 7;
 
         public OneDimensionalArray() {
-            array = new T[7];
+            array = new T[defaultCapacity];
+            size = 0;
         }
-
 
         public OneDimensionalArray(int n) {
             array = new T[n];
+            size = 0;
         }
-
 
         public override void Print()
         {
@@ -27,37 +30,22 @@ namespace Project4
             Console.WriteLine("");
         }
 
-        public bool CheckForEmptySlots(ref int index)
-        {
-            for (int i = 0; i < array.Length; i++)
-            {
-                if (array[i] == null)
-                {
-                    index = i;
-                    return true;
-                }
-            }
-            return false;
-        }
-
         public void AddElement(T newEl)
         {
-            int index = 0;
-            if (CheckForEmptySlots(ref index))
+            if (size >= array.Length)
             {
-                array[index] = newEl;
+                //T[] newArray = new T[_capacity];
+                //Array.CopyTo(newArray, 0, _array, _size);
+                //_array = newArray;
+                Array.Resize(ref array, array.Length*2+1);
             }
-            else
-            {
-                int oldLength = array.Length;
-                Array.Resize(ref array, array.Length * 2 + 1);
-                array[oldLength] = newEl;
-            }
+            array[size] = newEl;
+            size++;
         }
 
         public void DeleteElement()
         {
-
+            
         }
 
         public int NumberOfElements<C>()
@@ -112,21 +100,14 @@ namespace Project4
 
         public T FirstThatFollows(Predicate<T> conditionFunc)
         {
-            try
+            for (int i = 0; i < array.Length; i++)
             {
-                for (int i = 0; i < array.Length; i++)
+                if (conditionFunc(array[i]))
                 {
-                    if (conditionFunc(array[i]))
-                    {
-                        return array[i];
-                    }
+                    return array[i];
                 }
-                return array[0];
             }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
+            return array[0];
         }
 
         public void DoToEveryElement(Action<T> action)
@@ -143,7 +124,7 @@ namespace Project4
             int n = 0;
             for (int i = 0; i < array.Length; i++)
             {
-                if (!conditionFunc(array[i]))
+                if (conditionFunc(array[i]))
                 {
                     allThatFollow[n] = array[i];
                     n++;
@@ -178,14 +159,45 @@ namespace Project4
             }
         }
 
-        public void MinValue()
+        public T MinValue()
         {
-            
+            T min = array[0];
+            for(int i = 1; i < array.Length; i++)
+            {
+                if (min.CompareTo(array[i]) > 0)
+                {
+                    min = array[i];
+                }
+            }
+            return min;
         }
 
-        public void MaxValue()
+        public T MaxValue()
         {
+            T max = array[0];
+            for(int i = 1; i < array.Length; i++)
+            {
+                if (max.CompareTo(array[i]) < 0)
+                {
+                    max = array[i];
+                }
+            }
+            return max;
+        }
 
+        public void BubbleSort() {
+            T el;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j + 1 < array.Length - i; j++) {
+                    if (array[j + 1].CompareTo(array[j]) < 0)
+                    {
+                        el = array[j];
+                        array[j] = array[j+1];
+                        array[j+1] = el;
+                    }
+                }
+            }
         }
     }
 }
